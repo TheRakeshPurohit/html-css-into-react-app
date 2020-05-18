22501261f564f04472cf52fa569a17a35c3c4099
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-05-17 20:21:55 -0700

    Updates

---
+++ precache-manifest.93eab99c75731471523dc8062c9e2f3b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace0de19bfab77a2fff5def35e15b0f9",
+    "url": "/html-css-into-react-app/index.html"
+  },
+  {
+    "revision": "25933ba2786c535f91bb",
+    "url": "/html-css-into-react-app/static/css/main.ee2db946.chunk.css"
+  },
+  {
+    "revision": "430eb50601dd577116ee",
+    "url": "/html-css-into-react-app/static/js/2.8cd756d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/html-css-into-react-app/static/js/2.8cd756d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25933ba2786c535f91bb",
+    "url": "/html-css-into-react-app/static/js/main.3e167fa8.chunk.js"
+  },
+  {
+    "revision": "2427690a9f3aefddd915",
+    "url": "/html-css-into-react-app/static/js/runtime-main.b060ecd8.js"
+  },
+  {
+    "revision": "923f4d52f385a0eced03e9ed082e3076",
+    "url": "/html-css-into-react-app/static/media/blog.923f4d52.png"
+  },
+  {
+    "revision": "677f377a5e38cc8c7484839eddc2438a",
+    "url": "/html-css-into-react-app/static/media/blogFeatureImg.677f377a.jpg"
+  },
+  {
+    "revision": "d67f3b0f78a14aa2b01eafc9f1e72bee",
+    "url": "/html-css-into-react-app/static/media/blogFeatureImg2.d67f3b0f.jpg"
+  },
+  {
+    "revision": "4e4fbc44279ebd1d505a33a12c9b0dd9",
+    "url": "/html-css-into-react-app/static/media/blogFeatureImg3.4e4fbc44.jpg"
+  },
+  {
+    "revision": "127c4a3618e6a92fcededb4d55eb78c3",
+    "url": "/html-css-into-react-app/static/media/feature.127c4a36.jpg"
+  },
+  {
+    "revision": "41a02745f680918f1f7bc38d5ec83da8",
+    "url": "/html-css-into-react-app/static/media/section-3Img.41a02745.jpg"
+  },
+  {
+    "revision": "6ede9417ddbac809920abd987397afc4",
+    "url": "/html-css-into-react-app/static/media/section.6ede9417.jpg"
+  },
+  {
+    "revision": "227970d30cbfacbb53940bae5118c33d",
+    "url": "/html-css-into-react-app/static/media/team.227970d3.jpg"
+  },
+  {
+    "revision": "2c93205b6596e65bbc683e787aadab22",
+    "url": "/html-css-into-react-app/static/media/undraw_helpful_sign_s6al.2c93205b.svg"
+  }
+]);
